24132a0537b97a7ba1db1c85dc64b49bb0171b4f
Your Name <youremail@example.com>
2025-09-11 19:52:06 +0530

    add rate limmiting

--- post-gen/server/src/index.ts
+++ post-gen/server/src/index.ts
@@ -1,11 +1,12 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { imageRouter } from './Routes/Image-gen'
+import { ratelimmiter } from './middlewares/ratelimmiter'
 
 const app = new Hono()
 
 app.use(cors())
-app.get('/', (c) => {
+app.get('/', ratelimmiter, (c) => {
   return c.text('Hello Hono!')
 })
 app.route("/",imageRouter)
---
+++ post-gen/server/src/utils/ratelimmit.ts
@@ -0,0 +1,7 @@
+// import { Context } from "hono";
+
+// export class ratelimmit {
+//     private static instance : ratelimmit | null = null
+//     static async 
+
+// }
