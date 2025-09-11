8cae2af61ac1dd98be39e4086152bbf686c1803c
Your Name <youremail@example.com>
2025-09-11 19:52:40 +0530

    add rate limmiting

---
+++ share/db.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client"
+import { Redis } from "@upstash/redis/cloudflare"
+import { Context } from "hono"
+export class Singleton{
+    private static PrismaInstance : PrismaClient | null = null
+    private static redisInstance : Redis | null = null
+    static getPrismaInstance(){
+        console.log("crearing prisma")
+        if(!this.PrismaInstance){
+            this.PrismaInstance = new PrismaClient({
+                log: ["info"]
+            })
+        }
+        console.log("Done prisma")
+        return this.PrismaInstance
+    }
+    static getRedisInstance(c : Context){
+        console.log(c.env.REDIS_URL, c.env.REDIS_TOKEN)
+        if(!this.redisInstance){
+            this.redisInstance = new Redis({
+                url: c.env.REDIS_URL,
+                token: c.env.REDIS_TOKEN
+            })
+        }
+        console.log("done redis")
+        return this.redisInstance
+    }
+
+}
+
+// export const prisma = Singleton.getPrismaInstance()
